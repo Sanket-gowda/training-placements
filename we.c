ccc4f21ee18bb05d1389c8070e33c10ac5d4b977
Sanket <gowdasonu262@gmail.com>
2025-03-06 14:47:39 +0530

    change

--- we.c
+++ we.c
@@ -1,9 +1,9 @@
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-////int main()
-//{
-//    printf("Hello hh world!\n");
-//    return 0;
-//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+{
+   printf("Hello hh world!\n");
+   return 0;
+}
